9b146b7bd3fd5e34acb41f45ddaf67d6aac1eff1
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2021-06-14 20:40:12 +0530

    working ver 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const fetchData = (pageNumber) => {
+  const url = `https://randomuser.me/api?page=${pageNumber}`
+  console.log(url)
+  return axios.get(url)
+  .then(({ data }) => {
+    console.log(data);
+    return data;
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+}
 
 function App() {
+  const [userJSON, setUserJSON] = useState('');
+  const [userInfos, setUserInfos] = useState([]);
+  const [pageNumber, setPageNumber] = useState(0)
+
+  useEffect(() => {
+    fetchData(pageNumber).then((data) => {
+      setUserJSON(JSON.stringify(data));
+      const newUserInfos = [...userInfos, ...data.results];
+      setUserInfos(newUserInfos);
+      setPageNumber(pageNumber + 1);
+    })
+  }, [])
+
+  const fetchNextUser = () => {
+    fetchData(pageNumber).then((data) => {
+      setUserJSON(JSON.stringify(data));
+      const newUserInfos = [...userInfos, ...data.results];
+      setUserInfos(newUserInfos);
+      setPageNumber(pageNumber + 1);
+    })
+  } 
+
+  const buildUserName = (user) => {
+    const { name : {first, last}} = user;
+    return `${first} ${last}`;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => {
+        fetchNextUser();
+      }}>Load more</button>
+      {
+        userInfos.map((user, idx) => (
+          <div id={idx}>
+            <img src={user.picture.thumbnail} alt="" />
+            <p>{buildUserName(user)}</p>
+          </div>
+        ))
+      }
     </div>
   );
 }
